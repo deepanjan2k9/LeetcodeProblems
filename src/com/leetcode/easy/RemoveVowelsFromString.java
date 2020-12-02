c443bb672f52e19c0f0908e09b74bf3faaee5389
Deepanjan Das <30810534+deepanjan2k9@users.noreply.github.com>
2020-12-02 11:23:13 -0500

    Update RemoveVowelsFromString.java
    Completed solution and submitted to leetcode

--- src/com/leetcode/easy/RemoveVowelsFromString.java
+++ src/com/leetcode/easy/RemoveVowelsFromString.java
@@ -2,34 +2,40 @@ package com.leetcode.easy;
 
 public class RemoveVowelsFromString {
     public static void main(String[] args){
-        /*String s = "leetcodeisacommunityforcoders";
-        String s2 = "aeiou";*/
+        String s = "leetcodeisacommunityforcoders";
+        String s2 = "aeiou";
         String s3 = "leet";
         RemoveVowelsFromString obj = new RemoveVowelsFromString();
-        /*
+        
         System.out.println("Input: " + s);
         System.out.println("Result: " + obj.removeVowels(s));
         System.out.println("Input: " + s2);
-        System.out.println("Result: " + obj.removeVowels(s2));*/
+        System.out.println("Result: " + obj.removeVowels(s2));
         System.out.println("Input: " + s3);
         System.out.println("Result: " + obj.removeVowels(s3));
 
     }   
 
     public String removeVowels(String s){
-        String result = "";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
-        StringBuilder strBuilder = new StringBuilder(s);
-        for(int i=0; i<strBuilder.length(); i++){
-            if(strBuilder.charAt(i) == 'a' ||
-               strBuilder.charAt(i) == 'e' ||
-               strBuilder.charAt(i) == 'i' ||
-               strBuilder.charAt(i) == 'o' ||
-               strBuilder.charAt(i) == 'u'){
-                   strBuilder.deleteCharAt(i);
+        final int LENGTH = s.length();
+        String result = ""; 
+        char[] charArray = new char[LENGTH];                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+        
+        for(int i=0,j=0; i<LENGTH; i++){
+
+            if(s.charAt(i) == 'a' ||
+               s.charAt(i) == 'e' ||
+               s.charAt(i) == 'i' ||
+               s.charAt(i) == 'o' ||
+               s.charAt(i) == 'u'){
+                   continue;
+               }else {
+                   charArray[j] = s.charAt(i);
+                   j++;
                }
-               System.out.println("StringBuilder at iteration: " + i + " after removal: " + strBuilder.toString());
         }
-        result = strBuilder.toString();
+       
+        result = String.valueOf(charArray).trim();
 
         return result;
     }
