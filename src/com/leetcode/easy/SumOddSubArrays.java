36cafdbaa1567375a27c2a68475c90180ef24aa6
Deepanjan Das <30810534+deepanjan2k9@users.noreply.github.com>
2020-12-04 16:09:25 -0500

    Create SumOddSubArrays.java
    Initial commit

---
+++ src/com/leetcode/easy/SumOddSubArrays.java
@@ -0,0 +1,19 @@
+/**
+ * Given an array of positive integers arr, calculate the sum of all possible odd-length subarrays.
+ * A subarray is a contiguous subsequence of the array.
+ * Return the sum of all odd-length subarrays of arr.
+ */
+
+
+package com.leetcode.easy;
+
+public class SumOddSubArrays {
+
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+
+        return sum;
+        
+    }
+    
+}
